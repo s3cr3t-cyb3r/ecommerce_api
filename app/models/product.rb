e82670939a5285d3fc951f7cb2707fcf30c6e9fd
s3cr3t-cyb3r <lancesanchez2010@gmail.com>
2026-09-06 21:41:39 +1000

    initial commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+
+    def supplier
+        Supplier.find_by(id: supplier_id)
+    end
 end
